59b02dd87082f84ee7370d971cd24b3271dd74af
Slimcent <33299753+Slimcent@users.noreply.github.com>
2021-10-27 13:06:31 +0100

    All fies

---
+++ AgeCheck/Module1.vb
@@ -0,0 +1,26 @@
+﻿Module Module1
+
+    Sub Main()
+        Try
+            'Dim age As Integer
+            Dim age As String
+
+            Console.WriteLine("Enter Age")
+            age = Console.ReadLine()
+            While String.IsNullOrEmpty(age) Or IsNumeric(age) = False
+                Console.WriteLine("Age is Invalid")
+                age = Console.ReadLine()
+            End While
+
+            If age >= 18 Then
+                Console.WriteLine(" Congrats, You are Eligible to Vote")
+            Else
+                Console.WriteLine("Sorry! You are not Eligible to Vote")
+            End If
+        Catch ex As Exception
+            Console.WriteLine(ex.Message)
+        End Try
+
+    End Sub
+
+End Module
